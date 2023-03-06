abd99acce30492bbc14ace688f18acc31ff8233d
Andrew Lim <andrewlim10@gmail.com>
2023-03-05 19:59:12 -0800

    Kaplan Meier

---
+++ jupyter/work/Analysis.py
@@ -0,0 +1,144 @@
+import getpass
+import psycopg2
+from psycopg2 import Error
+from pathlib import Path
+import numpy as np
+from datetime import datetime
+
+
+class Analysis:
+    def __init__(self):
+        '''Connect To Database'''
+        print("Connect To Database")
+        try:            
+            # Connect to an existing database
+            self.connection = psycopg2.connect(user=getpass.getpass(prompt='username:'),
+                                                password=getpass.getpass(prompt='encryption key:'),
+                                                host="clinical_db",
+                                                port="5432",
+                                                database="clinical",
+                                                sslmode='require',
+                                                # Schemas
+                                                options="-c search_path=clinical_document,rad_onc,public")
+
+            # Print PostgreSQL details
+            print(f"Connected: {self.connection.get_dsn_parameters()}")
+        except (Exception, Error) as error:
+            print("Error while connecting to PostgreSQL: ", error)
+        return
+    
+    def __del__(self):
+        '''Disconnect from Database'''
+        if hasattr(self, 'connection'):
+            self.connection.close()
+        return
+    
+    # Get Start Date
+    def get_start_date(self, mrn, tag):
+        # Connect to DB
+        cursor = self.connection.cursor()
+
+        sql_ = f"SELECT attribute->>'date' as date, attribute->>'start_date' as start_date, attribute->>'end_date' as end_date \
+                  FROM clinical_document.q_document \
+                  WHERE  \
+                  mrn = '{mrn}' \
+                  AND tag='{tag}' \
+                  LIMIT 1;"
+        cursor.execute(sql_)
+        dates = np.array(cursor.fetchone()).flatten()
+        # Remove None and covert to datetime
+        dates = [datetime.strptime(date, '%Y-%m-%d').date() for date in dates if date is not None]
+        dates.sort()
+
+        # Start Date Does Not Exist
+        if len(dates) == 0:
+            date =  None
+        else:
+            date = dates[0]
+
+        # Close Cursor
+        cursor.close()
+        return date
+
+      # Get Event Date
+    
+    def get_event_date(self, mrn, tag):
+        # Connect to DB
+        cursor = self.connection.cursor()
+
+        # Get Positive Event Date
+        sql_ = f"SELECT attribute->>'date' as date \
+                  FROM clinical_document.q_document \
+                  WHERE tag = '{tag}' AND mrn='{mrn}' \
+                  ORDER BY attribute->>'date' ASC \
+                  LIMIT 1;"
+        cursor.execute(sql_)
+        date = np.array(cursor.fetchone()).flatten()[0]
+
+        if date != None:
+            # Convert to datetime
+            date = datetime.strptime(date, '%Y-%m-%d').date()
+            # Event True tag
+            event=True
+
+        else:
+            # If No positive Get Last Event
+            sql_ = f"SELECT attribute->>'date' as date, attribute->>'start_date' as start_date, attribute->>'end_date' as end_date \
+                  FROM clinical_document.q_document \
+                  WHERE mrn = '{mrn}' \
+                    AND (attribute->>'date' IS NOT NULL \
+                    OR attribute->>'start_date' IS NOT NULL \
+                    OR attribute->>'end_date' IS NOT NULL) \
+                  ORDER BY attribute->>'date' DESC;"
+            cursor.execute(sql_)
+            dates = np.array(cursor.fetchall()).flatten()
+            # Remove None and convert to datetime
+            dates = [datetime.strptime(date, '%Y-%m-%d').date() for date in dates if date is not None and date != '']
+
+            # Get last Occurance
+            dates.sort()
+            date = dates[-1]
+
+            # Event is False
+            event=False
+
+        # Close Cursor
+        cursor.close()
+        return date, event
+    
+    def kaplan_meier(self, mrns, start_tag, event_tag):
+        """
+        Generate Kaplan Meier Curves
+        mrns: <list>
+        start_tag: <string>
+        event_tag: <string>: 
+        """
+        # Get MRNs
+        durations = []
+        events = []
+        remove_idx = []
+        for idx, mrn in enumerate(mrns):
+            #print(f"{mrn=}", flush=True)
+            start_date = self.get_start_date(mrn, start_tag)
+            #print(f"{start_date=}", flush=True)
+            # Check if start date exists
+            if start_date == None:
+                remove_idx.append(idx)
+                continue
+
+            #print(f"{event=}", flush=True)
+            event_date, event_ = self.get_event_date(mrn, event_tag)
+            #print(f"{event_date=}", flush=True)
+
+            durations.append((event_date - start_date).days)
+            events.append(event_)
+            #print(f"{mrn=}", f"{start_date=}", f"{event_date=}", f"{event=}", flush=True)
+
+        # Remove MRNs without start event
+        mrns = np.delete(mrns, remove_idx).tolist()
+        #print(f"{durations=}", flush=True)
+        #print(f"{events=}", flush=True)
+
+        output = {'mrns':mrns, 'durations':durations, 'events':np.asarray(events).astype(int).tolist()}
+
+        return output
--- jupyter/work/Database.py
+++ jupyter/work/Database.py
@@ -1,9 +1,40 @@
 import getpass
 import pandas as pd
-
+import numpy as np
 import psycopg2
 from psycopg2 import Error
 from pathlib import Path
+
+
+# build OR sql query
+def build_logical_or_sql_query(column, values):
+  phase1 = f'\' OR {column}=\''.join(values)
+  sql = f"{column}=\'{phase1}\'"
+  return sql
+
+# build OR sql query
+def build_logical_OR_LIKE_sql_query(column, values):
+  phase1 = f'\' OR {column} LIKE \''.join(values)
+  sql = f"{column} LIKE \'{phase1}\'"
+  return sql
+
+# build AND sql query
+def build_logical_AND_sql_query(column, values):
+  phase1 = f'\' AND {column}=\''.join(values)
+  sql = f"{column}=\'{phase1}\'"
+  return sql
+# build AND sql query
+def build_logical_AND_LIKE_sql_query(column, values):
+  phase1 = f'\' AND {column}=\''.join(values)
+  sql = f"{column}=\'{phase1}\'"
+  return sql
+
+# build OR sql jsonb query
+def build_logical_OR_LIKE_sql_json_query(attribute, values):
+  phase1 = f'\' OR attribute->>\'{attribute}\' LIKE \''.join(values)
+  sql = f"attribute->>\'{attribute}\' LIKE \'{phase1}\'"
+  return sql
+
 
 class Database:
     def __init__(self):
@@ -82,3 +113,68 @@ class Database:
         data_df = pd.DataFrame(list(map(lambda a : a[1], data)), index = index)
         data_df = data_df.sort_index()
         return data_df
+    
+    def get_mrns_where_tag_value(self, data_options):
+        # Connect to DB
+        cursor = self.connection.cursor()
+
+        tags = np.asarray([json['tag'] for json in data_options if 'tag' in json ])
+        tags = np.unique(tags)
+
+        # Get MRNS when only tags are specified, and no attributes
+        mrns_no_attributes = None
+        tags_no_attributes = [json['tag'] for json in data_options if ('tag' in json and 'attribute' not in json)]
+        # Data without attributes
+        if len(tags_no_attributes) > 0:
+            sql = ''
+            for idx, tag in enumerate(tags_no_attributes):
+                if idx != 0: sql = sql + " INTERSECT "
+                sql = sql + f" SELECT mrn FROM clinical_document.q_document WHERE tag LIKE '{tag}' "
+
+            #print(f"{sql=}")
+            cursor.execute(sql)
+            mrns_ = np.unique(np.asarray(cursor.fetchall()).flatten())
+            if mrns_no_attributes is None:  mrns_no_attributes = mrns_
+            else: mrns_no_attributes = np.append(mrns_no_attributes, mrns_)
+
+
+        # Loop through tags
+        mrns_attributes = None
+        for idx, tag in enumerate(tags):
+            attributes = [json['attribute'] for json in data_options if ('tag' in json and 'attribute' in json and json['tag'] == tag)]
+
+            # Data with attributes
+            sql = 'SELECT DISTINCT mrn FROM clinical_document.q_document WHERE '
+            for idy, attribute in enumerate(attributes):
+                # sub_tags = np.asarray([json['sub_tag'] for json in data_options if json['tag'] == tag])
+                values = np.asarray([json['value'] for json in data_options if ('tag' in json and 'attribute' in json and 'value' in json and json['tag'] == tag and json['attribute'] == attribute)]).flatten()
+                if len(values) == 0: values=['%']
+                #print(f"{values=}")
+
+
+                if idy == 0: sql += f" tag LIKE '{tag}' "
+                else: sql += f" AND tag LIKE '{tag}' "
+
+                sql += f"AND ({build_logical_OR_LIKE_sql_json_query(attribute, values)})"
+                #print(f"{sql=}")
+                cursor.execute(sql)
+                mrns_ = np.unique(np.asarray(cursor.fetchall()).flatten())
+
+                if mrns_attributes is None: mrns_attributes = mrns_
+                else: mrns_attributes = np.intersect1d(mrns_attributes, mrns_)
+
+        # Close Cursor
+        cursor.close()
+
+        # Intersection
+        mrns = None
+        if mrns_no_attributes is not None:
+            mrns = mrns_no_attributes
+            if mrns_attributes is not None:
+                mrns = np.intersect1d(mrns, mrns_attributes)
+        else:
+            if mrns_attributes is not None:
+                mrns = mrns_attributes
+
+        #print(f"end:{mrns}", flush=True)
+        return np.unique(mrns)
